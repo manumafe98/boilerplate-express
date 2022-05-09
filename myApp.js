33c43fd29aacd38df9ebaf986fdcc7104a342830
Manuel <manumaxerapro@gmail.com>
2022-05-09 18:00:58 -0300

    json

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 app.use('/public' , express.static(__dirname + '/public'));
-
+app.get("/json", function(req, res){res.json({"message": "Hello json"})})
 
 
 
