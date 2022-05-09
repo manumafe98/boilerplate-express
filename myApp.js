72223ad379c6509704ebcbbaf95cd772634c45c1
Manuel <manumaxerapro@gmail.com>
2022-05-09 18:26:31 -0300

    json en mayus

--- myApp.js
+++ myApp.js
@@ -1,8 +1,16 @@
 let express = require('express');
+require('dotenv').config()
 let app = express();
 app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 app.use('/public' , express.static(__dirname + '/public'));
-app.get("/json", function(req, res){res.json({"message": "Hello json"})})
+
+app.get("/json", function(req, res){ 
+    if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON" })
+    }else {
+        res.json({"message": "Hello Json" })
+    }
+})
 
 
 
