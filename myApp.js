9994d81f6de5c3a0714e1720f59a7c8181f7d791
Manuel <manumaxerapro@gmail.com>
2022-05-09 18:28:55 -0300

    json en mayus

--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ app.get("/json", function(req, res){
     if (process.env.MESSAGE_STYLE == "uppercase"){
         res.json({"message": "HELLO JSON" })
     }else {
-        res.json({"message": "Hello Json" })
+        res.json({"message": "Hello json" })
     }
 })
 
