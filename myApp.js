8e837304f4a9943b8cff836a19b131f2ee23bb98
Manuel <manumaxerapro@gmail.com>
2022-05-09 17:25:42 -0300

    node

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 var bGround = require('fcc-express-bground');
 bGround.log("Hello world");
-app.get("/", function(req, res){res.send("Hello Express")});
+app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 
 
 
