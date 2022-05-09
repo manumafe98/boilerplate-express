3f349416461f1278588aaffe1550f3ca42b3b49f
Manuel <manumaxerapro@gmail.com>
2022-05-09 17:27:59 -0300

    cambios

--- myApp.js
+++ myApp.js
@@ -1,7 +1,5 @@
 let express = require('express');
 let app = express();
-var bGround = require('fcc-express-bground');
-bGround.log("Hello world");
 app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 
 
