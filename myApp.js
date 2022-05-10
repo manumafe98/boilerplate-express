55f95c422f010527f9c797e2fbf177b0e9cb3c87
Manuel <manumaxerapro@gmail.com>
2022-05-10 19:33:37 -0300

    Get Route Parameter Input from the Client

--- myApp.js
+++ myApp.js
@@ -2,6 +2,7 @@ let express = require('express');
 require('dotenv').config()
 let app = express();
 
+//Chain Middleware to Create a Time Server
 app.get('/now', function(req, res, next){
     req.time = new Date().toString();
     next();
@@ -11,23 +12,31 @@ function(req, res){
     res.send({time: req.time})
 })
 
-// app.use(function (req, res, next){
-//     console.log(req.method + " " + req.path  + " - "  + req.ip);
-//     next();
-// })
+//Get Route Parameter Input from the Client
+app.get('/:word/echo', function(req, res){
+    var word = req.params.word;
+    res.json(req.params.word)
+})
+
+//Implement a Root-Level Request Logger Middleware
+app.use(function (req, res, next){
+    console.log(req.method + " " + req.path  + " - "  + req.ip);
+    next();
+})
 
 
 app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 
 app.use('/public' , express.static(__dirname + '/public'));
 
-// app.get("/json", function(req, res){ 
-//     if (process.env.MESSAGE_STYLE == "uppercase"){
-//         res.json({"message": "HELLO JSON" })
-//     }else {
-//         res.json({"message": "Hello json" })
-//     }
-// })
+//Use the .env File --> require('dotenv').config() y tuvimos que instalar npm install dotenv
+app.get("/json", function(req, res){ 
+    if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({"message": "HELLO JSON" })
+    }else {
+        res.json({"message": "Hello json" })
+    }
+})
 
 
 
