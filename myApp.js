1fe29ce8b1f495626bddd12ee015588d27783922
Manuel <manumaxerapro@gmail.com>
2022-05-10 18:38:19 -0300

    implement root level request

--- myApp.js
+++ myApp.js
@@ -4,14 +4,18 @@ let app = express();
 app.get("/", function(req, res){res.sendFile('/views/index.html' , { root : __dirname })});
 app.use('/public' , express.static(__dirname + '/public'));
 
-app.get("/json", function(req, res){ 
-    if (process.env.MESSAGE_STYLE == "uppercase"){
-        res.json({"message": "HELLO JSON" })
-    }else {
-        res.json({"message": "Hello json" })
-    }
-})
+// app.get("/json", function(req, res){ 
+//     if (process.env.MESSAGE_STYLE == "uppercase"){
+//         res.json({"message": "HELLO JSON" })
+//     }else {
+//         res.json({"message": "Hello json" })
+//     }
+// })
 
+app.use(function(req, res, next){
+    console.log(req.method, req.path - req.ip);
+    next();
+})
 
 
 
